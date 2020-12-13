9076a0593b8c113a086ef795d10032625c7cadb2
dndbekfrl1 <dndbekfrl1@gmail.com>
2020-12-14 03:54:52 +0900

    search 구현 ...

--- main.js
+++ main.js
@@ -40,7 +40,7 @@ function deleteAfter2Weeks() {
       });
   }
 }
-setInterval(deleteAfter2Weeks(), 100000);
+setInterval(deleteAfter2Weeks, 100000);
 
 app.set("view engine", "ejs");
 app.set("views", "./view");
@@ -69,27 +69,3 @@ app.get("/search", (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
-/*
-const express = require('express') //모듈 로드 
-const app = express()
-const port = 3000
-
-var fs = require('fs');
-var template = require('./template/main.html');
-
-//route, rounting: 사용자의 path에 응답을 해줌 
-//경로, 경로가 호출됐을때 호출될 함수
-app.get('/', (request, response) => {
-  response.render('hello');
-});
-
-app.get('/page', (req, res) => {
-    res.send('/page')
-});
-
-//3000포트로 listen 
-//성공시   
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
-*/
