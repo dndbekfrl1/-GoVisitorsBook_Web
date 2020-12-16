160cd8d1118f0e318486bfd6360861edabf52791
dndbekfrl1 <dndbekfrl1@gmail.com>
2020-12-16 18:41:34 +0900

    setOff 수정

--- main.js
+++ main.js
@@ -24,36 +24,38 @@ app.use(
  */
 // [] ajax로 작동확인ㅎ
 // [V]작동확인
+var today = new Date();
+var hours = today.getHours();
+var minutes = today.getMinutes();
+
 function deleteAfter2Weeks() {
   console.log("execute!..");
-  var today = new Date();
-  var hours = today.getHours();
-  var minutes = today.getMinutes();
   const txName = Config.txActionName.setOff.txName;
   const from = Config.walletAddress.from;
   const apiKey = Config.dapp.apiKey;
 
-  if (hours === 0 && minutes >= 0 && minutes <= 10) {
-    var data = {
-      from: `${from}`,
-    };
-    axios
-      .post(`https://api.luniverse.io/tx/v1.1/transactions/${txName}`, data, {
-        headers: {
-          Authorization: `Bearer ${apiKey}`,
-          "Content-Type": "application/json",
-        },
-      })
-      .then(() => {
-        console.log("2주 전의 데이터가 삭제되었습니다.");
-      })
-      .catch((err) => {
-        console.log("error발새어1!!!");
-        console.log(err);
-      });
-  }
+  var data = {
+    from: `${from}`,
+  };
+  axios
+    .post(`https://api.luniverse.io/tx/v1.1/transactions/${txName}`, data, {
+      headers: {
+        Authorization: `Bearer ${apiKey}`,
+        "Content-Type": "application/json",
+      },
+    })
+    .then(() => {
+      console.log("2주 전의 데이터가 삭제되었습니다.");
+    })
+    .catch((err) => {
+      console.log("error발생");
+      console.log(err);
+    });
 }
-setInterval(deleteAfter2Weeks, 1000);
+
+if (hours === 0 && minutes >= 0 && minutes <= 10) {
+  setInterval(deleteAfter2Weeks, 1000);
+}
 app.set("view engine", "ejs");
 app.set("views", "./view");
 
